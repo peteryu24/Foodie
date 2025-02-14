d88f16851b9b59769372394c1246f07c906e98a6
haisley77 <hahi1108@gmail.com>
2025-02-14 15:30:15 +0900

    feat: 예외 코드 및 반환 코드 구성

---
+++ src/main/java/com/sparta/tl3p/backend/common/type/ErrorCode.java
@@ -0,0 +1,36 @@
+package com.sparta.tl3p.backend.common.type;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+    // Member
+    MEMBER_NOT_FOUND("M-001", "Member Not Found"),
+    EMAIL_NOT_FOUND("M-002", "Member Email Not Found"),
+    ROLE_NOT_FOUND("M-003", "Member Role Not Found"),
+
+    // Store
+
+    // Review
+
+    // Item
+
+    // Order
+
+    // Payment
+
+    // Valid
+    ARGUMENT_NOT_VALID("V-001", "Method Argument Not Valid"),
+
+    // Unexpected Exception
+    UNEXPECTED_ERROR("D-001", "Unexpected error ");
+
+
+    private final String code;
+    private final String message;
+
+    ErrorCode(final String code, final String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
