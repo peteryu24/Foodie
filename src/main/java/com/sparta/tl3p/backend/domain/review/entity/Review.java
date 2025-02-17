76fc300cc2303f239297e5396f844b4ebf4dc684
haisley77 <hahi1108@gmail.com>
2025-02-17 12:09:17 +0900

    mod: Review 엔티티 BaseEntity 반영, setter 제거

--- src/main/java/com/sparta/tl3p/backend/domain/review/entity/Review.java
+++ src/main/java/com/sparta/tl3p/backend/domain/review/entity/Review.java
@@ -1,23 +1,20 @@
 package com.sparta.tl3p.backend.domain.review.entity;
 
+import com.sparta.tl3p.backend.common.audit.BaseEntity;
 import com.sparta.tl3p.backend.domain.order.entity.Order;
 import com.sparta.tl3p.backend.domain.store.entity.Store;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 @Table(name = "p_review")
-public class Review {
+public class Review extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long reviewId;
 
     @Column(nullable = false)
@@ -25,17 +22,6 @@ public class Review {
 
     @Column(nullable = false)
     private String content;
-
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt = LocalDateTime.now();
-
-    private String createdBy;
-
-    private LocalDateTime updatedAt;
-    private String updatedBy;
-
-    private LocalDateTime deletedAt;
-    private String deletedBy;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
@@ -49,4 +35,7 @@ public class Review {
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
 
+    private void updateStatus(ReviewStatus reviewStatus) {
+        this.status = reviewStatus;
+    }
 }
