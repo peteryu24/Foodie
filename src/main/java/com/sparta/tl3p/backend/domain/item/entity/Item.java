8ea69c348c25249adac43d70f2af88b7b82fd819
aerhergag00 <jwlee101500@gmail.com>
2025-02-17 18:57:57 +0900

    fix: Item 엔티티 수정
    nullable 해제
    @SQLRestriction 추가

--- src/main/java/com/sparta/tl3p/backend/domain/item/entity/Item.java
+++ src/main/java/com/sparta/tl3p/backend/domain/item/entity/Item.java
@@ -2,11 +2,13 @@ package com.sparta.tl3p.backend.domain.item.entity;
 
 import com.sparta.tl3p.backend.common.audit.BaseEntity;
 import com.sparta.tl3p.backend.domain.item.enums.ItemStatus;
+import com.sparta.tl3p.backend.domain.store.entity.Store;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -15,6 +17,7 @@ import java.util.UUID;
 @Table(name = "p_item")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLRestriction("status != 'DELETED'")
 public class Item extends BaseEntity {
 
     @Id
@@ -35,19 +38,19 @@ public class Item extends BaseEntity {
     @Column(name = "status")
     private ItemStatus status = ItemStatus.ACTIVE;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+
     @Builder
-    public Item(String name, BigDecimal price, String description) {
+    public Item(Store store, String name, BigDecimal price, String description) {
+        this.store = store;
         this.name = name;
         this.price = price;
         this.description = description;
     }
 
-    @PrePersist
-    public void generateId() {
-        if (this.id == null) {
-            this.id = UUID.randomUUID();
-        }
-    }
 
     public void updateItem(String name, BigDecimal price, String description, ItemStatus status) {
         this.name = name;
