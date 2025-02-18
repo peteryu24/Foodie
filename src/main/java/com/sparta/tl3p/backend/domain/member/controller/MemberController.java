b6f409c4f169d3262ef01d4a3a7619a676208501
이용재 <dydwo6018@gmail.com>
2025-02-18 15:10:59 +0900

    회원가입, 조회, 수정, 삭제 기능

--- src/main/java/com/sparta/tl3p/backend/domain/member/controller/MemberController.java
+++ src/main/java/com/sparta/tl3p/backend/domain/member/controller/MemberController.java
@@ -2,14 +2,17 @@ package com.sparta.tl3p.backend.domain.member.controller;
 
 import com.sparta.tl3p.backend.domain.member.dto.MemberRequestDto;
 import com.sparta.tl3p.backend.domain.member.dto.MemberResponseDto;
+import com.sparta.tl3p.backend.domain.member.entity.Member;
 import com.sparta.tl3p.backend.domain.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("api/v1/auth")
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class MemberController {
 
@@ -29,4 +32,23 @@ public class MemberController {
         return ResponseEntity.ok(memberService.getMemberById(memberId));
     }
 
+    //회원정보수정
+    @PutMapping("/{memberId}")
+    public ResponseEntity<MemberResponseDto> updateMember(@PathVariable Long memberId, @RequestBody MemberRequestDto requestDto){
+        return ResponseEntity.ok(memberService.updateMember(memberId, requestDto));
+    }
+
+    // 회원탈퇴
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity<String> deleteMember(@PathVariable Long memberId){
+        memberService.deleteMember(memberId);
+        return ResponseEntity.ok("회원 탈퇴가 완료되었습니다.");
+    }
+
+    // 회원전체조회
+    @GetMapping
+    public ResponseEntity<List<MemberResponseDto>> getAllMembers(){
+        List<MemberResponseDto> members = memberService.getAllMembers();
+        return ResponseEntity.ok(members);
+    }
 }
