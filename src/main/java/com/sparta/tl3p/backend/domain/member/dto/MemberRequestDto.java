0dcddd7bb5bdaeef79271ca04101f3eb7a7cc6ab
이용재 <dydwo6018@gmail.com>
2025-02-18 14:01:44 +0900

    import 삭제

--- src/main/java/com/sparta/tl3p/backend/domain/member/dto/MemberRequestDto.java
+++ src/main/java/com/sparta/tl3p/backend/domain/member/dto/MemberRequestDto.java
@@ -1,9 +1,7 @@
 package com.sparta.tl3p.backend.domain.member.dto;
 
 import com.sparta.tl3p.backend.domain.member.enums.Role;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
