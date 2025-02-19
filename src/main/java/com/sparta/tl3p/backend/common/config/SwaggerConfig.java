88d39dbb7dd91e29ed1c832101b8b3cef3135e20
aerhergag00 <jwlee101500@gmail.com>
2025-02-19 21:52:39 +0900

    feat: swagger 기본 설정

---
+++ src/main/java/com/sparta/tl3p/backend/common/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.sparta.tl3p.backend.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("AI Business Project")
+                        .description("Team 17")
+                        .version("v1")
+                )
+                .components(new Components()
+                        .addSecuritySchemes(HttpHeaders.AUTHORIZATION, new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization"))
+                )
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(HttpHeaders.AUTHORIZATION)
+                );
+    }
+}
