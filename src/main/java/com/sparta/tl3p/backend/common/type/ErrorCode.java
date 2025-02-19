e48c707593bd450ee1b7ceff488f6a166b6610d2
이용재 <dydwo6018@gmail.com>
2025-02-19 20:40:12 +0900

    비밀번호 불일치 에러코드 추가 유효하지않은 토큰 에러코드 추가

--- src/main/java/com/sparta/tl3p/backend/common/type/ErrorCode.java
+++ src/main/java/com/sparta/tl3p/backend/common/type/ErrorCode.java
@@ -1,6 +1,7 @@
 package com.sparta.tl3p.backend.common.type;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
@@ -9,6 +10,8 @@ public enum ErrorCode {
     EMAIL_NOT_FOUND("M-002", "Member Email Not Found"),
     ROLE_NOT_FOUND("M-003", "Member Role Not Found"),
     USERNAME_DUPLICATE("M-004", "Username already exists"),
+    PASSWORD_MISMATCH("M-005", "비밀번호가 일치하지 않습니다"),
+    INVALID_JWT_TOKEN("M-006", "유효하지 않은 JWT 토큰입니다."),
 
     // Store
 
