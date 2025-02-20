fe59147cbe5b0320180d61c262cd12603af5ca14
이용재 <dydwo6018@gmail.com>
2025-02-20 11:18:22 +0900

    SecurityConfig

---
+++ src/main/java/com/sparta/tl3p/backend/common/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.sparta.tl3p.backend.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request
+                                .requestMatchers("/api/v1/**").permitAll()
+                                .anyRequest().authenticated()
+                        );
+        return http.build();
+
+    }
+
+}
