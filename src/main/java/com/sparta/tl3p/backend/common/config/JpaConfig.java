fcb3c3461e6a134cef635bea1ee907b348c163aa
aerhergag00 <jwlee101500@gmail.com>
2025-02-21 15:04:43 +0900

    feat: jpa 생성자/수정자/삭제자 추적

--- src/main/java/com/sparta/tl3p/backend/common/config/JpaConfig.java
+++ src/main/java/com/sparta/tl3p/backend/common/config/JpaConfig.java
@@ -1,10 +1,16 @@
 package com.sparta.tl3p.backend.common.config;
 
+import com.sparta.tl3p.backend.common.audit.AuditorAwareImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
 public class JpaConfig {
-
+    @Bean
+    public AuditorAware<Long> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
 }
