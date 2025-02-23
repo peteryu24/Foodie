763cbfcde0e88c7adcf01ea4c75ca5e0318022ee
aerhergag00 <jwlee101500@gmail.com>
2025-02-24 02:10:42 +0900

    add: Mockito 테스트를 위한 DTO 수정

--- src/main/java/com/sparta/tl3p/backend/domain/ai/dto/AIDescriptionRequestDto.java
+++ src/main/java/com/sparta/tl3p/backend/domain/ai/dto/AIDescriptionRequestDto.java
@@ -1,11 +1,15 @@
 package com.sparta.tl3p.backend.domain.ai.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AIDescriptionRequestDto {
     @NotNull
     private UUID itemId;
