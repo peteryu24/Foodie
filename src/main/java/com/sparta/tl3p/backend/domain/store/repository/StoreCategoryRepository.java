50b0a57cf690da245e33d496e4b2de5aafa3e914
peteryu24 <67302252+peteryu24@users.noreply.github.com>
2025-02-25 10:27:20 +0900

    불필요한 선언문 삭제

--- src/main/java/com/sparta/tl3p/backend/domain/store/repository/StoreCategoryRepository.java
+++ src/main/java/com/sparta/tl3p/backend/domain/store/repository/StoreCategoryRepository.java
@@ -6,12 +6,9 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface StoreCategoryRepository extends JpaRepository<StoreCategory, UUID> {
-
-    List<StoreCategory> findByStoreId(UUID storeId);
 
     @Transactional
     @Modifying
